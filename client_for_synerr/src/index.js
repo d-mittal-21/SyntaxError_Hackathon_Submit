20dfbbe181f79042162caa233cc60cb790bc22d7
Divyansh Mittal <dmittal21@iitk.ac.in>
2023-01-14 19:44:50 +0530

    update basic frontend

--- client_for_synerr/src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import { ReactDOM } from 'react';
-import {BrowserRouter as Router} from 'react-router-dom';
-import { chainId, ThirdwebProvider} from '@thirdweb-dev/react';
-
-const root = ReactDOM.createRoot(document.getElementById('root'));
-
-root.render()
