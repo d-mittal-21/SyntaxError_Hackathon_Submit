98c65cd3a5e902387f7fa07e333281ee60d2c61c
Divyansh Mittal <dmittal21@iitk.ac.in>
2023-01-14 10:47:52 +0530

    deploy smart contract and start frontend

---
+++ client_for_synerr/src/index.js
@@ -0,0 +1,8 @@
+import React from 'react';
+import { ReactDOM } from 'react';
+import {BrowserRouter as Router} from 'react-router-dom';
+import { chainId, ThirdwebProvider} from '@thirdweb-dev/react';
+
+const root = ReactDOM.createRoot(document.getElementById('root'));
+
+root.render()
